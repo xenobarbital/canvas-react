1a36106975b700188d3177eae21ae2f8a382cf7f
xenobarbital <greencross.am@gmail.com>
2023-01-23 21:44:10 +0400

    retrieve pixel data from mouse pointer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 import beach from './assets/tropical_beach.jpg';
 
 function App() {
-  const [width, setWidth] = useState(1920);
-  const [height, setHeight] = useState(1080);
+  // Main canvas location
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+
   const imageRef = useRef(null);
   const canvasRefOne = useRef(null);
 
   const canvasSize = useMemo(() => ({
-    height: height > 4000 ? 4000 : height,
-    width: width > 4000 ? 4000 : width,
-  }), [height, width]);
+    height: 1080,
+    width: 1920,
+  }), []);
+
+  // get coordinates of canvas element
+  const getCanvasPosition = () => {
+    const x = canvasRefOne.current.offsetLeft;
+    setX(x);
+
+    const y = canvasRefOne.current.offsetTop;
+    setY(y);
+  };
 
   useEffect(() => {
     const canvasOne = canvasRefOne.current;
@@ -19,12 +31,36 @@ function App() {
     const image = imageRef.current;
 
     image.addEventListener("load", ({target: img}) => {
-      setHeight(img.height);
-      setWidth(img.width);
-
+      const { width, height } = canvasSize;
       ctxOne.drawImage(image, 0, 0, width, height, 0, 0, width, height);
     });
-  }, [height, width])
+  }, [canvasSize])
+
+  // get the initial position of canvas in the beginning
+  useEffect(() => {
+    getCanvasPosition();
+  }, []);
+
+  // Re-calculate X and Y of the canvas when the window is resized by the user
+  useEffect(() => {
+    window.addEventListener("resize", getCanvasPosition);
+  }, []);
+
+  // get event coordinates
+  const getEventLocation = (element, event) => ({
+    x: event.pageX - x,
+    y: event.pageY - y,
+  })
+
+
+  // handle pointer hovering over canvas
+  const mouseMoveHandle = (e) => {
+    const canvasOne = canvasRefOne.current;
+    const ctxOne = canvasOne.getContext('2d');
+    const eventLocation = getEventLocation(canvasOne, e)
+    const { data } = ctxOne.getImageData(eventLocation.x, eventLocation.y, 1, 1);
+    console.log('pixel data', data)
+  }
 
   return (
     <>
@@ -33,6 +69,7 @@ function App() {
         <img ref={imageRef} src={beach} alt="beach"/>
       </div>
       <canvas
+        onMouseMove={mouseMoveHandle}
         ref={canvasRefOne}
         className='canvasBig'
         {... canvasSize}
